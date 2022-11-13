b711419b0177c55417bc6d700c6306485dec485d
Radyslav_Shovkivskyi <r.shovkivskyi@gmail.com>
2022-11-13 23:57:15 +0200

    Added actions to counter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
+import React from 'react'
 import './index.scss';
 
 function App() {
+  const [count, setCount] = React.useState(0)
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{count}</h1>
+        <button className="minus" onClick={()=>setCount(count - 1)}>- Минус</button>
+        <button className="plus" onClick={()=>setCount(count + 1)}>Плюс +</button>
       </div>
     </div>
   );
